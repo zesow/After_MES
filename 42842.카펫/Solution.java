6192e2e320cab5bf5af0210c553f7f635831e422
zesow <anstkd07@gmail.com>
2020-09-07 17:06:56 +0900

    42842 initial commit

--- 42842.카펫/Solution.java
+++ 42842.카펫/Solution.java
@@ -1,7 +1,20 @@
 class Solution {
     
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        
+        for(int yellowHeight = 1 ; yellowHeight <= yellow; ++yellowHeight) {
+            
+            if(yellow % yellowHeight == 0) {
+                
+                if((yellowHeight + 2) * ((yellow / yellowHeight) + 2) == brown + yellow) {
+                    answer[0] = (yellow / yellowHeight) + 2;
+                    answer[1] = yellowHeight + 2;
+                    
+                    break;
+                }
+            }
+        }
         
         return answer;
     }
