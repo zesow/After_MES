14e8b5d482bebd558af75567cec1c64c233e8f20
zesow <anstkd07@gmail.com>
2020-09-08 18:05:01 +0900

    42583 ms 풀이

--- 42583.다리를 지나는 트럭/Solution.java
+++ 42583.다리를 지나는 트럭/Solution.java
@@ -1,8 +1,52 @@
+import java.util.*;
+
 class Solution {
-    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int[] answer = 0;
+        int answer = 0;
         
-        return answer;
+        Queue<Integer> truckQueue = new LinkedList<Integer>();
+        Queue<Integer> timeQueue = new LinkedList<Integer>();
+        
+        truckQueue.add(truck_weights[0]);
+        timeQueue.add(0);
+        
+        int nowTrucksWeight = truck_weights[0];
+        int truckIdx = 1;
+        
+        while(!truckQueue.isEmpty()) {
+            
+            ++answer;
+            
+            int queueSize = 0;
+            // 빼준다
+            while(!timeQueue.isEmpty()) {
+                
+                if(timeQueue.peek() + 1 == bridge_length) {
+                    timeQueue.poll();
+                    nowTrucksWeight -= truckQueue.poll();
+                }
+                else{
+                    queueSize = timeQueue.size();
+                    for(int i = 0 ; i < queueSize; ++i) {
+                        timeQueue.add(timeQueue.poll() + 1);
+                    }
+                    break;
+                }
+            }
+            
+            // 넣어준다       
+            if(truckIdx < truck_weights.length && nowTrucksWeight + truck_weights[truckIdx] <= weight) {
+                truckQueue.add(truck_weights[truckIdx]);
+                nowTrucksWeight += truck_weights[truckIdx];
+                timeQueue.add(0);
+                    
+                truckIdx++;
+                
+            }
+            
+        }
+        
+        
+        return answer + 1;
     }
 }
