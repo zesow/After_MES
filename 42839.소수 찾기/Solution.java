729f0232f89004e9a733ae6a23026d04a36c3a8b
zesow <anstkd07@gmail.com>
2020-09-10 15:23:31 +0900

    42839 ms

--- 42839.소수 찾기/Solution.java
+++ 42839.소수 찾기/Solution.java
@@ -1,8 +1,57 @@
+import java.util.*;
+
 class Solution {
-    
     public int solution(String numbers) {
-        int answer = 0;
         
-        return answer;
+        char[] numArr = numbers.toCharArray();
+        boolean[] check = new boolean[numArr.length];
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int i = 0 ; i < numArr.length; ++i) {
+            check[i] = true;
+            dfs(numArr,check, i, "",set);
+            check[i] = false;
+        }
+        
+        return set.size();
+    }
+    
+    private void dfs(char[] numArr,boolean[] check,int idx, String numStr,HashSet<Integer> set) {
+        
+        numStr += numArr[idx];
+        
+        int num = Integer.parseInt(numStr);
+        
+        if(isPrime(num)) {
+            set.add(num);
+        }
+        
+        for(int i = 0 ; i < numArr.length; ++i) {
+            if(!check[i]) {
+                check[i] = true;
+                dfs(numArr,check,i,numStr,set);
+                check[i] = false;
+            }
+        }
+        
+        numStr = numStr.substring(0,numStr.length() - 1);
+    }
+    
+    
+    private boolean isPrime(int num) {
+        
+        if(num < 2) return false;
+        
+        int n = 2;
+        
+        while(n < num) {
+            if(num % n == 0) 
+                return false;
+            
+            ++n;
+        }
+        
+        return true;
+            
     }
 }
