c91ee84243acc6b5eb4101d1d0ef1adf82781134
zesow <anstkd07@gmail.com>
2020-09-11 19:50:51 +0900

    12899 ms

--- 12899.124 나라의 숫자/Solution.java
+++ 12899.124 나라의 숫자/Solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public String solution(int n) {
+        String answer = "";
         
+        while(n > 0) {
+            if(n % 3 == 0) {
+                answer += "4";
+                n -= 1;
+            }
+            else {
+                answer += String.valueOf(n % 3);
+            }
+            
+            n /= 3;
+        }
+        
+        return new StringBuffer(answer).reverse().toString();
     }
 }
